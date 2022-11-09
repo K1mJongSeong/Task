64cf9e878edd2f88e61f692988b21e072a337ab7
JongSeong Kim <miaer456@naver.com>
2022-11-09 18:04:25 +0900

    2022/11/9

--- lib/ui/add_task_bar.dart
+++ lib/ui/add_task_bar.dart
@@ -15,6 +15,21 @@ class _AddTaskPageState extends State<AddTaskPage> {
   DateTime  _selectedDate = DateTime.now();
   String _endTime = "9:30 PM";
   String _startTime = DateFormat("hh:mm a").format(DateTime.now()).toString();
+  int _selectedRemind = 5;
+  List<int> remindList = [
+    5,
+    10,
+    15,
+    20,
+  ];
+  String _selectedRepeat = "None";
+  List<String> repeatList = [
+    "None",
+    "Daily",
+    "Weekly",
+    "Monthly",
+  ];
+  int _selectedColor = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -69,8 +84,88 @@ _getTimeFromUser(isStartTime: true);
                           ),
                         ),
                       )),
-              ],)
-
+              ],),
+              MyInputField(title: "Remind", hint: "$_selectedRemind minutes early",
+              widget: DropdownButton(
+                icon: Icon(Icons.keyboard_arrow_down,
+                color: Colors.grey),
+                iconSize: 32,
+                elevation: 4,
+                style: subHeadingStyle,
+                onChanged: (String? newValue){
+                  setState(() {
+                    _selectedRemind = int.parse(newValue!);
+                  });
+                },
+                underline: Container(height: 0,),
+                items: remindList.map<DropdownMenuItem<String>>((int value){
+                  return DropdownMenuItem<String>(
+                    value: value.toString(),
+                    child: Text(value.toString()),
+                  );
+                }
+                ).toList(),
+              ),
+              ),
+              MyInputField(title: "Repeat", hint: "$_selectedRepeat",
+                widget: DropdownButton(
+                  icon: Icon(Icons.keyboard_arrow_down,
+                      color: Colors.grey),
+                  iconSize: 32,
+                  elevation: 4,
+                  style: subHeadingStyle,
+                  onChanged: (String? newValue){
+                    setState(() {
+                      _selectedRepeat = newValue!;
+                    });
+                  },
+                  underline: Container(height: 0,),
+                  items: repeatList.map<DropdownMenuItem<String>>((String? value){
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value!,style: TextStyle(color: Colors.grey)),
+                    );
+                  }
+                  ).toList(),
+                ),
+              ),
+              SizedBox(height: 18,),
+              Row(
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text("Color",
+                      style: titleStyle,
+                      ),
+                      SizedBox(height: 8.0,),
+                      Wrap(
+                        children: List<Widget>.generate(
+                            3,
+                                (int index) {
+                              return GestureDetector(
+                                onTap: (){
+                                  setState(() {
+                                    _selectedColor=index;
+                                    print("$index");
+                                  });
+                                },
+                                child: Padding(
+                          padding: const EdgeInsets.only(right: 8.0),
+                          child: CircleAvatar(
+                                radius: 14,
+                            backgroundColor: index==0?primaryClr:index==1?pinkClr:yellowClr,
+                            child: _selectedColor==index?Icon(Icons.done,color: Colors.white,size: 16,):Container(),
+                          ),
+                        ),
+                              );
+                        }
+                        )
+                      ),
+                    ],
+                  )
+                ],
+              )
             ],
           ),
         ),
