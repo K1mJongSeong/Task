f131db2564abf03a8722489498b079404d9e1178
JongSeong Kim <miaer456@naver.com>
2022-11-16 17:21:21 +0900

    2022/11/16

--- lib/services/notification_services.dart
+++ lib/services/notification_services.dart
@@ -1,5 +1,6 @@
 
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:flutter_native_timezone/flutter_native_timezone.dart';
 import 'package:get/get.dart';
 import 'package:timezone/timezone.dart' as tz;
 import 'package:timezone/data/latest.dart' as tz;
@@ -10,7 +11,7 @@ class NotifyHelper{
     FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
 
     initializeNotification() async {
-      tz.initializeTimeZones();
+      _configureLocalTimezone();
       final IOSInitializationSettings   initializationSettingsIOS =
       IOSInitializationSettings (
           requestSoundPermission: false,
@@ -26,7 +27,7 @@ class NotifyHelper{
       await flutterLocalNotificationsPlugin.initialize(initializationSettings, onSelectNotification: selectNotification);
     }
 
-    displayNotification({required String title, required String body}) async {
+    Future<void> displayNotification({required String title, required String body}) async {
       print("doing test");
       var androidPlatformChannelSpecifics = new AndroidNotificationDetails(
           'your channel id', 'your channel name',channelDescription: 'your channel description',
@@ -46,17 +47,36 @@ class NotifyHelper{
     scheduledNotification(int hour, int minutes, Task task) async {
       int newTime = 5;
       await flutterLocalNotificationsPlugin.zonedSchedule(
-          0,
-          'scheduled title',
-          'theme changes 5 seconds ago',
-          tz.TZDateTime.now(tz.local).add(const Duration(seconds: 5)),
+          task.id!.toInt(),
+          task.title,
+          task.note,
+          _convertTime(hour, minutes),
+          //tz.TZDateTime.now(tz.local).add(const Duration(seconds: 5)),
           const NotificationDetails(
               android: AndroidNotificationDetails('your channel id',
                   'your channel name',channelDescription: 'asd')),
           androidAllowWhileIdle: true,
           uiLocalNotificationDateInterpretation:
-          UILocalNotificationDateInterpretation.absoluteTime);
+          UILocalNotificationDateInterpretation.absoluteTime,
+      matchDateTimeComponents: DateTimeComponents.time);
 
+    }
+    
+    tz.TZDateTime _convertTime(int hour, int minutes) {
+      final tz.TZDateTime now = tz.TZDateTime.now(tz.local);
+      tz.TZDateTime scheduleDate =
+          tz.TZDateTime(tz.local,now.year,now.month,now.day, hour, minutes);
+      if(scheduleDate.isBefore(now)){
+        scheduleDate = scheduleDate.add(const Duration(days: 1));
+      }
+
+      return scheduleDate;
+    }
+
+    Future<void> _configureLocalTimezone() async {
+      tz.initializeTimeZones();
+      final String timeZone = await FlutterNativeTimezone.getLocalTimezone();
+      tz.setLocalLocation(tz.getLocation(timeZone));
     }
 
     void requestIOSPermissions() {
